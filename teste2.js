46b1ce9fa85a6a7d7a7f63fcb8810eeb7625f222
joaovillarb <36734606+joaovillarb@users.noreply.github.com>
2021-04-22 19:58:52 -0300

    melhorias

--- teste2.js
+++ teste2.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById("canvas")
 const context = canvas.getContext('2d');
 
+const getRange = length => [...Array(length).keys()]
+
 const girdSize = 10;
 
 let CanvasWidth;
@@ -18,6 +20,7 @@ let score;
 let level;
 
 let speed;
+let velocidade;
 
 let active = true;
 
@@ -32,6 +35,7 @@ function initCanvas() {
     level = 0;
     score = 0;
     speed = 100;
+    velocidade = speed;
     active = true;
     direction = "RIGHT"
     map = null;
@@ -40,12 +44,10 @@ function initCanvas() {
     canvas.width = WIDTH;
     canvas.height = HEIGHT;
 
-    // document.body.appendChild(canvas);
-    generateGrid();
+    generateMap();
 
     generateSnake();
     generateFood();
-    console.log(map)
 
     drawGame();
 
@@ -54,85 +56,94 @@ function initCanvas() {
 
 function drawGame() {
     context.clearRect(0, 0, canvas.width, canvas.height);
-    console.log(map[snake[0].x][snake[0].y])
 
-    // for (var i = snake.length - 1; i >= 0; i--) {
-    //     if (i === 0) {
-    //         switch (direction) {
-    //             case "RIGHT": // Right
-    //                 snake[0] = { x: snake[0].x + 1, y: snake[0].y }
-    //                 console.log("case")
-    //                 break;
-    //             case "LEFT": // Left
-    //                 snake[0] = { x: snake[0].x - 1, y: snake[0].y }
-    //                 break;
-    //             case "UP": // Up
-    //                 snake[0] = { x: snake[0].x, y: snake[0].y - 1 }
-    //                 break;
-    //             case "DOWN": // Down
-    //                 snake[0] = { x: snake[0].x, y: snake[0].y + 1 }
-    //                 break;
-    //         }
+    // renderCells();
 
-    //         if (snake[0].x < 0 ||
-    //             snake[0].x >= xLineTotal ||
-    //             snake[0].y < 0 ||
-    //             snake[0].y >= yLineTotal) {
-    //             console.log('if 2')
-    //             showGameOver();
-    //             return;
-    //         }
+    for (var i = snake.length - 1; i >= 0; i--) {
+        if (i === 0) {
+            switch (direction) {
+                case "RIGHT": // Right
+                    snake[0] = { x: snake[0].x + 1, y: snake[0].y }
+                    break;
+                case "LEFT": // Left
+                    snake[0] = { x: snake[0].x - 1, y: snake[0].y }
+                    break;
+                case "UP": // Up
+                    snake[0] = { x: snake[0].x, y: snake[0].y - 1 }
+                    break;
+                case "DOWN": // Down
+                    snake[0] = { x: snake[0].x, y: snake[0].y + 1 }
+                    break;
+            }
 
-    //         if (map[snake[0].x][snake[0].y] === 1) {
-    //             score += 10;
-    //             generateFood();
+            if (snake[0].x < 0 ||
+                snake[0].x >= xLineTotal ||
+                snake[0].y < 0 ||
+                snake[0].y >= yLineTotal) {
+                console.log('if 2')
+                showGameOver();
+                return;
+            }
 
-    //             snake.push({ x: snake[snake.length - 1].x, y: snake[snake.length - 1].y });
-    //             map[snake[snake.length - 1].x][snake[snake.length - 1].y] = 2;
 
-    //             if ((score % 100) === 0) {
-    //                 level += 1;
-    //             }
+            //CASO ATINJA A COMIDA
+            if (map[snake[0].x][snake[0].y] === "fruta") {
+                score += 10;
+                generateFood();
 
-    //             // Caso a cabeça atinga outra parte do corpo, finaliza o jogo
-    //         } else if (map[snake[0].x][snake[0].y] === 2) {
-    //             console.log('else if')
-    //             console.log(map[snake[0].x][snake[0].y])
-    //             console.log(snake[0].x)
-    //             console.log(snake[0].y)
-    //             showGameOver();
-    //             return;
-    //         }
+                snake.push({ x: snake[snake.length - 1].x, y: snake[snake.length - 1].y });
+                map[snake[snake.length - 1].x][snake[snake.length - 1].y] = "cobra";
 
-    //         map[snake[0].x][snake[0].y] = 2;
-    //     } else {
-    //         if (i === (snake.length - 1)) {
-    //             map[snake[i].x][snake[i].y] = null;
-    //         }
+                if ((score % 100) === 0) {
+                    level += 1;
+                }
 
-    //         snake[i] = { x: snake[i - 1].x, y: snake[i - 1].y };
-    //         map[snake[i].x][snake[i].y] = 2;
-    //     }
-    // }
+
+            }
+            // Caso a cabeça atinga outra parte do corpo, finaliza o jogo
+            else if (map[snake[0].x][snake[0].y] === 2) {
+                console.log('else if')
+                // console.log(map[snake[0].x][snake[0].y])
+                // console.log(snake[0].x)
+                // console.log(snake[0].y)
+                showGameOver();
+                return;
+            }
+
+            map[snake[0].x][snake[0].y] = 2;
+        } else {
+            console.log('else')
+            if (i === (snake.length - 1)) {
+                map[snake[i].x][snake[i].y] = null;
+            }
+
+            console.log('CABEÇA COBRA X -' + snake[0].x)
+            console.log('CORPO COBRA X -' + snake[i].x)
+            console.log('CABEÇA COBRA Y -' + snake[0].y)
+            console.log('CORPO COBRA Y -' + snake[i].y)
+
+            snake[i] = { x: snake[i - 1].x, y: snake[i - 1].y };
+            map[snake[i].x][snake[i].y] = "cobra";
+        }
+    }
 
     drawMain();
 
     // Start cycling the matrix
     for (var x = 0; x < map.length; x++) {
         for (let y = 0; y < map[0].length; y++) {
-            if (map[x][y] === 1) {
+            if (map[x][y] === "fruta") {
                 context.fillStyle = 'red';
                 context.fillRect(x * girdSize, y * girdSize, 10, 10);
-            } else if (map[x][y] === 2) {
+            } else if (map[x][y] === "cobra") {
                 context.fillStyle = '#6bd82b';
-                context.fillRect((x * girdSize) + 2, y * girdSize, 10, 10);
+                context.fillRect(x * girdSize, y * girdSize, 10, 10);
             }
         }
     }
 
     if (active) {
-        let velocidade = speed - (level * 10);
-        console.log(velocidade)
+        velocidade = speed - (level * 10);
         setTimeout(drawGame, velocidade);
     }
 }
@@ -140,30 +151,36 @@ function drawGame() {
 function keyDownHandler(event) {
     let key = event.key;
     if (key === 'ArrowUp' && direction !== "DOWN") {
-        console.log('UP')
+        // console.log('UP')
         direction = "UP"; // Up
     } else if (key === 'ArrowDown' && direction !== "UP") {
-        console.log('DOWN')
+        // console.log('DOWN')
         direction = "DOWN"; // Down
     } else if (key === 'ArrowLeft' && direction !== "RIGHT") {
-        console.log('LEFT')
+        // console.log('LEFT')
         direction = "LEFT"; // Left
     } else if (key === 'ArrowRight' && direction !== "LEFT") {
-        console.log('RIGHT')
+        // console.log('RIGHT')
         direction = "RIGHT"; // Right
     }
 }
 
+const renderCells = () => {
+    context.globalAlpha = 0.15
+    getRange(CanvasWidth).forEach(column => getRange(CanvasHeight).forEach(row => {
+        if ((column + row) % 2 === 1) {
+            context.fillStyle = '#000000';
+            context.fillRect(column * girdSize, row * girdSize, girdSize, girdSize)
+        }
+    }))
+    context.globalAlpha = 1
+}
+
 function drawMain() {
-    // context.lineWidth = 2;
-    // context.strokeStyle = 'black';
-
-    // context.strokeRect(2, 20, canvas.width - 2, canvas.height - 20);
-
     context.fillStyle = 'black';
-    context.font = '12px sans-serif';
-    context.fillText('Score ' + score, score > 0 ? score > 90 ? canvas.height - 60 : canvas.height - 55 : canvas.height - 50, canvas.width - 5);
-    context.fillText('Level ' + level, 2, canvas.width - 5);
+    context.font = '15px sans-serif';
+    context.fillText(score, score > 0 ? score > 90 ? canvas.width - 25 : canvas.width - 20 : canvas.width - 13, 15);
+    context.fillText('Level ' + level, 2, canvas.height - 5);
 }
 
 function generateSnake() {
@@ -176,11 +193,9 @@ function generateSnake() {
         rndX = Math.round(Math.random() * xLineTotal);
     }
 
-    console.log(rndX)
-    console.log(snake.length)
     for (let i = 0; i < snake.length; i++) {
         snake[i] = { x: rndX - i, y: rndY };
-        map[rndX - i][rndY] = 2;
+        map[rndX - i][rndY] = "cobra";
     }
 }
 
@@ -210,30 +225,18 @@ function generateFood() {
     // Também precisamos vigiar para não colocar a comida
     // na mesma posição da matriz ocupada por uma parte do
     // corpo da cobra.
-    while (map[rndX][rndY] === 2) {
+    while (map[rndX][rndY] === "cobra") {
         rndX = Math.round(Math.random() * (xLineTotal - 1));
         rndY = Math.round(Math.random() * (yLineTotal - 1));
     }
 
-    map[rndX][rndY] = 1;
+    map[rndX][rndY] = "fruta";
 }
 
 function generateRandomXY() {
     rndX = Math.round(Math.random() * (xLineTotal - 1));
     rndY = Math.round(Math.random() * (yLineTotal - 1));
 }
-
-function resizeCanvas() {
-    WIDTH = 600;//window.innerWidth;
-    HEIGHT = 600;//window.innerHeight;
-
-    canvas.width = WIDTH;
-    canvas.height = HEIGHT;
-
-    document.body.appendChild(canvas);
-    generateGrid();
-}
-
 
 window.onload = function () {
     let largura = document.getElementById('campo1')
@@ -249,32 +252,13 @@ window.onload = function () {
     document.getElementById('start').click()
 }
 
-function generateGrid() {
+function generateMap() {
 
     CanvasWidth = context.canvas.width;
     CanvasHeight = context.canvas.height;
 
     xLineTotal = Math.floor(CanvasWidth / girdSize); // Calcula o número de linhas do eixo x
     yLineTotal = Math.floor(CanvasHeight / girdSize); // Calcula o número de linhas do eixo y
-
-
-    // console.log('linha - ' + xLineTotal);
-    // for (let j = 0; j < xLineTotal; j++) {
-    //     context.beginPath(); // Open the path and set different styles
-    //     context.moveTo(girdSize * j, 0);
-    //     context.lineTo(girdSize * j, CanvasHeight);
-    //     context.strokeStyle = "#ccc"; // Set the color of each line
-    //     context.stroke();
-    // }
-
-    // console.log('coluna - ' + yLineTotal);
-    // for (let i = 0; i < yLineTotal; i++) {
-    //     context.beginPath(); // Open the path and set different styles
-    //     context.moveTo(0, girdSize * i - 0.5); // -0.5 is to solve the problem of pixel blur
-    //     context.lineTo(CanvasWidth, girdSize * i - 0.5);
-    //     context.strokeStyle = "#ccc"; // Set the color of each line
-    //     context.stroke();
-    // }
 
     map = new Array(xLineTotal);
     for (let i = 0; i < map.length; i++) {
